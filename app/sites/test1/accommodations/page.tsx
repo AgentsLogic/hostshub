9740eb3869c0fa09958748e70405b8a1527aeff9
AgentsLogic <agentslogic@gmail.com>
2025-04-21 16:37:44 -0500

    Fix import paths for Vercel build after reset

--- app/sites/test1/accommodations/page.tsx
+++ app/sites/test1/accommodations/page.tsx
@@ -1,5 +1,5 @@
-import { Accommodations } from '../../pages/Accommodations';
-import { Layout } from '../../components/Layout';
+import { Accommodations } from '../../../templates/project/src/pages/Accommodations';
+import { Layout } from '../../../templates/project/src/components/Layout';
 
 export default function Page() {
   return (
